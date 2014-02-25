13db9edd5d08dcded8a1e8eafdd690ddebf1db55
Adam Lazzarato <adam.lazzarato@gmail.com>
2014-02-24 22:28:23 -0500

    Quotes, capitalization, arithmetic

--- spool.sh
+++ spool.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 stop() {
     echo " Stopping."
-    rm -rf $TMP_DIR # remove data file directory
+    rm -rf "$TMP_DIR" # remove data file directory
     exit 0
 }
 trap stop SIGINT
@@ -11,83 +11,82 @@ trap stop SIGINT
 # $1 = file in $TMP_DIR/
 # $2 = value (hashrate in Mhash/s or shares)
 graph() {
-    echo $2 >> $TMP_DIR/$1 # append latest API response to file
+    echo "$2" >> "$TMP_DIR/$1" # append latest API response to file
 
     # if label and sparkline are wider than the terminal width
-    if [[ `cat $TMP_DIR/$1 | wc -l | awk '{print $1}'` -gt `expr $WIDTH-$LABELS-1` ]]; then
-        mv $TMP_DIR/$1 $TMP_DIR/$1-old
-        touch $TMP_DIR/$1
-        sed '1d' $TMP_DIR/$1-old >> $TMP_DIR/$1 # remove top line
-        rm $TMP_DIR/$1-old
+    if [[ `cat "$TMP_DIR/$1" | wc -l | awk '{print $1}'` -gt $(($WIDTH-$LABELS-1)) ]]; then
+        mv "$TMP_DIR/$1" "$TMP_DIR/$1-old"
+        touch "$TMP_DIR/$1"
+        sed '1d' "$TMP_DIR/$1-old" >> "$TMP_DIR/$1" # remove top line
+        rm "$TMP_DIR/$1-old"
     fi
-    DATA=`cat $TMP_DIR/$1 | tr '\n' ' '`
-    echo $DATA | spark
+    data=`cat "$TMP_DIR/$1" | tr '\n' ' '`
+    echo "$data" | spark
     printf "$2" # no new line (print units after function call)
 }
 just_graph() {
-    DATA=`cat $TMP_DIR/$1 | tr '\n' ' '`
-    echo $DATA | spark
+    data=`cat "$TMP_DIR/$1" | tr '\n' ' '`
+    echo "$data" | spark
     printf "$2" # no new line (print units after function call)
 }
 
 main() {
-    JSON=`curl --silent https://mining.bitcoin.cz/accounts/profile/json/$API_KEY`
+    json=`curl --silent https://mining.bitcoin.cz/accounts/profile/json/$API_KEY`
 
-    HASHRATE_TIME_ELAPSED=$HASHRATE_REFRESH_TIME
-    RESET_HASHRATE_TIME=1
+    hashrate_time_elapsed=$HASHRATE_REFRESH_TIME
 
-    mkdir $TMP_DIR # create data file directory
+    mkdir "$TMP_DIR" # create data file directory
 
     # create data files
-    cat $CONFIG_FILE | while read -r LINE || [ -n "$LINE" ]; do
-        if [[ "$LINE" != "" && "$LINE" != "$API_KEY" \
-            && "$LINE" != "newline" ]]; then
+    cat "$CONFIG_FILE" | while read -r line || [ -n "$line" ]; do
+        if [[ "$line" != "" && "$line" != "$API_KEY" \
+            && "$line" != "newline" ]]; then
 
             # touch worker data files
-            if [[ "$LINE" == "last_share" || "$LINE" == "score" \
-                || "$LINE" == "alive" || "$LINE" == "worker_hashrate" \
-                || "$LINE" == "worker_shares" ]]; then
+            if [[ "$line" == "last_share" || "$line" == "score" \
+                || "$line" == "alive" || "$line" == "worker_hashrate" \
+                || "$line" == "worker_shares" ]]; then
 
                 # determine how many workers exist
-                DATA=`echo $JSON | jq -r '.workers' | jq -r .[].$LINE`
+                data=`echo $json | jq -r '.workers' | jq -r .[].$line`
 
                 # create a file for each worker
                 i=0
-                for ENTRY in $DATA; do
-                    WORKER=`echo $JSON | jq -r '.workers | keys['$i']'`
-                    touch "$TMP_DIR/$WORKER-$LINE"
-                    i=`expr $i + 1`
+                for entry in $data; do
+                    worker=`echo $json | jq -r '.workers | keys['$i']'`
+                    touch "$TMP_DIR/$worker-$line"
+                    (( i++ ))
                 done
 
             # touch non-worker data files
             else
-                touch $TMP_DIR/$LINE
+                touch $TMP_DIR/$line
             fi
         fi
     done
 
     while [ 1 ]; do
-        JSON=`curl --silent https://mining.bitcoin.cz/accounts/profile/json/$API_KEY`
+        json=`curl --silent https://mining.bitcoin.cz/accounts/profile/json/$API_KEY`
         clear
 
         printf "###########\n#  SPool  #   [Ctrl+C] to stop.\n###########\n"
 
-        if [ $HASHRATE_TIME_ELAPSED -ge $HASHRATE_REFRESH_TIME ]; then
-            REFRESH_HASHRATE=1
-            HASHRATE_TIME_ELAPSED=0
+        if [ $hashrate_time_elapsed -ge $HASHRATE_REFRESH_TIME ]; then
+            refresh_hashrate=1
+            hashrate_time_elapsed=0
         fi
 
         # read config file
-        cat $CONFIG_FILE | while read -r LINE || [ -n "$LINE" ]; do
-            case $LINE in
+        cat $CONFIG_FILE | while read -r line || [ -n "$line" ]; do
+            case $line in
                 # non-worker info
                 "hashrate"*) # total hashrate
                     printf "hashrate: "
-                    if [ $REFRESH_HASHRATE -eq 1 ]; then
-                        HASHRATE=`echo $JSON | jq -r .$LINE`
+                    if [ $refresh_hashrate -eq 1 ]; then
+                        HASHRATE=`echo $json | jq -r .$line`
                         graph "hashrate" $HASHRATE
                     else
-                        HASHRATE=`echo $JSON | jq -r .$LINE`
+                        HASHRATE=`echo $json | jq -r .$line`
                         just_graph "hashrate" $HASHRATE
                     fi
                     printf " Mhash/s\n"
@@ -96,45 +95,44 @@ main() {
                 "nmc_send_threshold"|"confirmed_reward"|"wallet"| \
                 "unconfirmed_nmc_reward"|"unconfirmed_reward"| \
                 "estimated_reward"*) # general key/value
-                    DATA=`echo $JSON | jq -r .$LINE`
-                    printf "$LINE: $DATA\n"
+                    data=`echo $json | jq -r .$line`
+                    printf "$line: $data\n"
                 ;;
 
                 # worker info
                 "last_share"|"score"|"alive"*) # general key/value
-                    INFO=`echo $JSON | jq -r '.workers' | jq -r .[].$LINE`
-                    printf "$LINE\n"
+                    info=`echo $json | jq -r '.workers' | jq -r .[].$line`
+                    printf "$line\n"
 
                     i=0
-                    for ENTRY in $INFO; do
-                        WORKER=`echo $JSON | jq -r '.workers | keys['$i']'`
-                        printf "$WORKER: $ENTRY\n"
-                        i=`expr $i + 1`
+                    for entry in $info; do
+                        worker=`echo $json | jq -r '.workers | keys['$i']'`
+                        printf "$worker: $entry\n"
+                        (( i++ ))
                     done
                 ;;
                 "worker_hashrate"|"worker_shares"*)
-                    NAME=`echo $LINE | cut -d'_' -f 2`
-                    DATA=`echo $JSON | jq -r '.workers' | jq -r .[].$NAME`
+                    name=`echo $line | cut -d'_' -f 2`
+                    data=`echo $json | jq -r '.workers' | jq -r .[].$name`
 
-                    printf "$LINE\n"
+                    printf "$line\n"
 
                     i=0
-                    for ENTRY in $DATA; do
-                        WORKER=`echo $JSON | jq -r '.workers | keys['$i']'`
-                        printf "$WORKER\n"
-                        i=`expr $i + 1`
-                        if [ "$LINE" == "worker_hashrate" ]; then
+                    for entry in $data; do
+                        worker=`echo $json | jq -r '.workers | keys['$i']'`
+                        printf "$worker\n"
+                        (( i++ ))
+                        if [ "$line" == "worker_hashrate" ]; then
                             printf "hashrate: "
-                            if [ $REFRESH_HASHRATE -eq 1 ]; then
-                                graph "$WORKER-worker_hashrate" $ENTRY
-                                RESET_HASHRATE_TIME=1
+                            if [ $refresh_hashrate -eq 1 ]; then
+                                graph "$worker-worker_hashrate" $entry
                             else
-                                just_graph "$WORKER-worker_hashrate" $ENTRY
+                                just_graph "$worker-worker_hashrate" $entry
                             fi
                             printf " Mhash/s\n"
                         else
                             printf "shares:   "
-                            graph "$WORKER-worker_shares" $ENTRY
+                            graph "$worker-worker_shares" $entry
                             printf " shares\n"
                         fi
                     done
@@ -145,17 +143,17 @@ main() {
                     printf "\n";
                 ;;
                 *)
-                    printf "\033[31mUnknown option: $LINE.\033[0m"
+                    printf "\033[31mUnknown option: $line.\033[0m"
                 ;;
             esac
         done
         sleep $SLEEP_TIME
 
-        if [ $REFRESH_HASHRATE == 1 ]; then
-            REFRESH_HASHRATE=0
+        if [ $refresh_hashrate -eq 1 ]; then
+            refresh_hashrate=0
         fi
 
-        HASHRATE_TIME_ELAPSED=`expr $HASHRATE_TIME_ELAPSED + $SLEEP_TIME`
+        hashrate_time_elapsed=$(( $hashrate_time_elapsed + $SLEEP_TIME ))
     done
 }
 
